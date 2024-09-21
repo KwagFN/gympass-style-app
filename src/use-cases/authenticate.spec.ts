8012136162eeaae14fa855d46a3dc425e2555102
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-21 18:24:34 -0300

    test: it should not be able to authenticate with wrong email

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,6 +2,7 @@ import { expect, it, describe } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memmory-users-repository'
 import { AuthenticateUserUseCase } from './authenticate'
 import { hash } from 'bcrypt'
+import { InvalidUserCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate User Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -20,5 +21,17 @@ describe('Authenticate User Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const userRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUserUseCase(userRepository)
+
+    await expect(() =>
+      sut.execute({
+        email: 'john1@example.com',
+        password: '1234567890',
+      }),
+    ).rejects.toBeInstanceOf(InvalidUserCredentialsError)
   })
 })
