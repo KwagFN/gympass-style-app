4a629d8c77aacead59b3f589f500561cf5b85ed1
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-23 19:28:16 -0300

    feat: add refreshTokenController and test it should be able to refresh a token (e2e)

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { CreateUserController } from './register'
 import { AuthenticateUserController } from './authenticate'
 import { ProfileController } from './profile'
+import { refreshTokenController } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', CreateUserController)
   app.post('/sessions', AuthenticateUserController)
+
+  app.patch('/token/refresh', refreshTokenController)
 
   app.get('/me', { onRequest: [verifyJWT] }, ProfileController)
 }
