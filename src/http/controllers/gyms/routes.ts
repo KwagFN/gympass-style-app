6c6141b777f62c4159d9440f3116cf9ecf7c2e5f
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-23 14:00:18 -0300

    feat: add fetchNearbyGymsController

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,9 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { CreateGymController } from './create'
+import { SearchGymsController } from './search'
+import { fetchNearbyGymsController } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', SearchGymsController)
+  app.get('/gyms/nearby', fetchNearbyGymsController)
 
   app.post('/gym', CreateGymController)
 }
