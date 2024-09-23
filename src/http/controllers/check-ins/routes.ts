36017415942c13fee98f16aef53895b84319ca92
TheFNetto <franciscojdsnetto@gmail.com>
2024-09-23 14:27:15 -0300

    feat: add validateCheckInController

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,9 +3,16 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { createCheckInController } from './create'
+import { validateCheckInController } from './validate'
+import { checkInHistoryController } from './history'
+import { getUserMetricsController } from './metrics'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
+  app.get('/check-ins/history', checkInHistoryController)
+  app.get('/check-ins/metrics', getUserMetricsController)
+
   app.post('/gyms/:gymId/check-ins', createCheckInController)
+  app.patch('/check-ins/:checkInId/validate', validateCheckInController)
 }
